89a13c3d8ead220a255321c5f2de00738699e49a
Lajos Santa <santa.lajos@coldline.hu>
2015-04-05 09:48:37 +0200

    Add author to package and fix dependecy version

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'plutonium-plugin-output-transmission',
+    version = '1.0.1',
     description = 'Transmission output plugin for Plutonium',
-    version = '1.0',
+    author = 'Lajos Santa',
+    author_email = 'santa.lajos@coldline.hu',
+    url = 'https://github.com/voidpp/plutonium-plugin-output-transmission',
+    license = 'MIT',
     install_requires = [
         'plutonium == 1.0',
-        'transmissionrpc == 1.0',
+        'transmissionrpc == 0.11',
     ],
     include_package_data = True,
     packages = find_packages(),
 )
-
